2ccb5538fdb365ee6b2e0fb6e0e233d86336bb12
Nnamdi <nnamdi.nnah@routepayng.com>
2023-09-03 15:24:17 +0100

    added file-contectext to app.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import PrivacyPolicy from "./pages/privacy/privacy"
 import Cookies from "./pages/cookies-policy/cookies-policy"
 import Enduser from "./pages/eula/eula"
 import Alfiles from "./pages/allfiles"
+import { FileProvider } from "./FileContext"
 import {useRef} from "react"
 
 const App = () => {
   const faq = useRef(null);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing faq={faq}/>}></Route>
-        <Route exact path="/terms-and-condition" element={<TermsAndCondition faq={faq}/>}></Route>
-        <Route exact path="/privacy" element={<PrivacyPolicy />}></Route>
-        <Route exact path="/eula" element={<Enduser />}></Route>
-        <Route exact path="/cookies" element={<Cookies />}></Route>
-        <Route exact path="/list" element={<Alfiles />}></Route>
-
-      </Routes>
-    </Router>  
+    <FileProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing faq={faq}/>}></Route>
+            <Route exact path="/terms-and-condition" element={<TermsAndCondition faq={faq}/>}></Route>
+            <Route exact path="/privacy" element={<PrivacyPolicy />}></Route>
+            <Route exact path="/eula" element={<Enduser />}></Route>
+            <Route exact path="/cookies" element={<Cookies />}></Route>
+            <Route exact path="/list" element={<Alfiles />}></Route>
+          </Routes>
+        </Router>  
+    </FileProvider>
   );
 }
 
